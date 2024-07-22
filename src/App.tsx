e1eacec6222ab75cb339552529e1589c31a475e1
gAtrium <pdimetil@gmail.com>
2024-07-22 14:47:56 +0300

    add src

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import {
+  ConnectButton,
+  useCurrentAccount,
+  useSignAndExecuteTransaction,
+} from '@mysten/dapp-kit';
+import { WhRedeemModal, WhTransferModal } from '@zktx.io/wormhole-kit';
+import { enqueueSnackbar } from 'notistack';
+
+import type { IUnsignedTx } from '@zktx.io/wormhole-kit';
+
+import './App.css';
+
+function App() {
+  const { mutate: signAndExecuteTransactionBlock } =
+    useSignAndExecuteTransaction();
+  const account = useCurrentAccount();
+
+  const handleUnsignedTxs = (unsignedTxs: IUnsignedTx[]) => {
+    try {
+      const unsignedTx = unsignedTxs[0];
+      unsignedTx &&
+        signAndExecuteTransactionBlock(
+          {
+            transaction: unsignedTxs[0],
+          },
+          {
+            onError: (err) => {
+              enqueueSnackbar(err.message, {
+                variant: 'error',
+              });
+            },
+            onSuccess: (data) => {
+              enqueueSnackbar(data.digest, {
+                variant: 'success',
+              });
+            },
+          },
+        );
+    } catch (error) {
+      enqueueSnackbar(`${error}`, {
+        variant: 'error',
+      });
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={'/logo.png'} className="App-logo" alt="logo" />
+        <h1>Sui Example</h1>
+        {!account ? (
+          <ConnectButton />
+        ) : (
+          <span>
+            <WhTransferModal
+              chain="Sui"
+              token={'0x2::sui::SUI'}
+              address={account.address}
+              trigger={<button>Transfer</button>}
+              handleUnsignedTxs={handleUnsignedTxs}
+            />
+            &nbsp;
+            <WhRedeemModal
+              chain="Sui"
+              address={account.address}
+              trigger={<button>Redeem</button>}
+              handleUnsignedTxs={handleUnsignedTxs}
+            />
+          </span>
+        )}
+        <h2>@zktx.io/wormhole-kit</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,55 @@
+import { StrictMode } from 'react';
+
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui.js/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WhProvider } from '@zktx.io/wormhole-kit';
+import { SnackbarProvider } from 'notistack';
+import { createRoot } from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import '@mysten/dapp-kit/dist/index.css';
+
+const queryClient = new QueryClient();
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <StrictMode>
+    <WhProvider
+      network="Testnet"
+      chains={['Sui', 'Sepolia', 'Solana', 'Aptos', 'Celo', 'Polygon']}
+      config={{
+        chains: {
+          Ethereum: {
+            rpc: 'https://eth-goerli.public.blastapi.io',
+          },
+          Polygon: {
+            rpc: 'https://polygon-mumbai.api.onfinality.io/public',
+          },
+        },
+      }}
+      mode="dark"
+    >
+      <SnackbarProvider maxSnack={3}>
+        <QueryClientProvider client={queryClient}>
+        <SuiClientProvider
+          defaultNetwork="testnet"
+          networks={{ testnet: { url: getFullnodeUrl('testnet') } }}
+        >
+          <WalletProvider autoConnect>
+          <App />
+          </WalletProvider>
+        </SuiClientProvider>
+      </QueryClientProvider>
+      </SnackbarProvider>
+    </WhProvider>
+  </StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
